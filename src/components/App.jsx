46b4cb6c24e367d7e0557cf9dc8d8499d131e1fb
Ruslan Negoda <iarjuna108@gmail.com>
2022-11-08 16:38:44 +0200

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,67 @@
 import { Component } from 'react';
+import fetchPictures from './Api/Api';
 import { Div } from './App.styled';
-// import { LoadeMore } from './Button/Button';
+import { LoadeMore } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Spiner } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
   state = {
     search: '',
+    page: 1,
+    gallery: [],
+    isLoading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevSearch = prevState.search;
+    const prevPage = prevState.page;
+
+    const { search, gallery, page } = this.state;
+
+    // перевірка чи змінився пошук і чи змінилась сторінка, якщо щось змінилось фетчим
+    if (prevSearch !== search || prevPage !== page) {
+      this.setState({ isLoading: true });
+
+      try {
+        const data = fetchPictures(search, page);
+
+        console.log(data);
+        data.then(data => {
+          this.setState(prevState => {
+            return { gallery: prevState.gallery.concat(data.hits) };
+          });
+        });
+      } catch (error) {
+        this.setState({ error });
+      } finally {
+        this.setState({ isLoading: false });
+      }
+    }
+  }
+
+  // Ф-ція на кнопка дозавантаження картинок
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   // отримуємо значення з інпут форми пошуку
   onSearchValue = value => {
-    this.setState({ search: value.value });
+    this.setState({ search: value.value, page: 1, gallery: [] });
   };
 
   render() {
-    const { search } = this.state;
+    const { search, gallery } = this.state;
     return (
       <Div>
         <Searchbar onSubmit={this.onSearchValue} />
-        <ImageGallery searchValue={search} />
+        <ImageGallery gallery={gallery} />
+        {this.state.isLoading && <Spiner />}
+        {gallery.length >= 12 && <LoadeMore onClick={this.loadMore} />}
       </Div>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,79 +1,14 @@
-import axios from 'axios';
-import { LoadeMore } from 'components/Button/Button';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Spiner } from 'components/Loader/Loader';
-import { Component } from 'react';
 import { Gallery } from './ImageGallery.styled';
-
-export class ImageGallery extends Component {
-  state = {
-    page: 1,
-    gallery: [],
-    isLoading: false,
-  };
-
-  async getPictures() {
-    const config = {
-      method: 'get',
-      url: 'https://pixabay.com/api/',
-      params: {
-        key: '30077123-b07f3bce85b956a1421c5c012',
-        q: `${this.props.searchValue}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 12,
-        page: `${this.state.page}`,
-      },
-    };
-
-    const response = await axios(config);
-
-    const data = response.data;
-    return data;
-  }
-
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    // Умова чи змінились пропси, щоб не було зацикленості
-    if (prevProps.searchValue !== this.props.searchValue) {
-      this.setState({ page: 1, gallery: [] });
-    }
-
-    if (
-      prevProps.searchValue !== this.props.searchValue ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
-      this.getPictures()
-        .then(data => {
-          this.setState(prevState => {
-            return { gallery: prevState.gallery.concat(data.hits) };
-          });
-        })
-        .finally(() => this.setState({ isLoading: false }));
-    }
-  }
-
-  render() {
-    const { gallery } = this.state;
-    return (
-      <>
-        {this.state.isLoading && <Spiner />}
-        <Gallery>
-          {gallery.map(({ id, webformatURL }) => {
-            return (
-              <ImageGalleryItem key={id} smallImg={webformatURL} id={id} />
-            );
-          })}
-        </Gallery>
-        <LoadeMore onClick={this.loadMore} />
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ gallery }) => {
+  return (
+    <>
+      <Gallery>
+        {gallery.map(({ id, webformatURL }) => {
+          return <ImageGalleryItem key={id} smallImg={webformatURL} id={id} />;
+        })}
+      </Gallery>
+    </>
+  );
+  // }
+};
